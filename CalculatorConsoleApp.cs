1693f8a900051544c377726cf93fd68782f66b77
damar.glh__ <114411272+damar-glh@users.noreply.github.com>
2023-03-08 17:03:45 +0700

    Update CalculatorConsoleApp.cs

--- CalculatorConsoleApp.cs
+++ CalculatorConsoleApp.cs
@@ -32,4 +32,27 @@ namespace CalculatorConsoleApp
             b = int.Parse(Console.ReadLine());
         }
         
+        // program utama
+        static void Main(string[] args)
+        {
+            // variabel yang digunakan
+            int choice, a, b;
+
+            // array menu
+            string[] menu = { "Penambahan", "Pengurangan", "Perkalian", "Pembagian" };
+
+            Console.Title= "Aplikasi Calculator";
+            Console.WriteLine("Pilih Menu Calculator : \n");
+
+            // perulangan untuk menampilkan data array diatas
+            for (int i = 0; i < menu.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}. {menu[i]}");
+            }
+
+            Console.Write("\nInput nomor menu [1..4]: ");
+
+            // inputan
+            choice = int.Parse(Console.ReadLine());
+        
         
