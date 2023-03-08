cc5f502b4988d753433633f3cd31f24c55756d1a
damar.glh__ <114411272+damar-glh@users.noreply.github.com>
2023-03-08 17:02:51 +0700

    Create CalculatorConsoleApp.cs

---
+++ CalculatorConsoleApp.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace CalculatorConsoleApp
+{
+    class CalculatorConsoleApp
+    {
+        // membuat method 
+        static int Penambahan(int a, int b)
+        {
+            return a + b;
+        }
+        static int Pengurangan(int a, int b)
+        {
+            return a - b;
+        }
+        static int Perkalian(int a, int b)
+        {
+            return a * b;
+        }
+        static int Pembagian(int a, int b)
+        {
+            return a / b;
+        }
