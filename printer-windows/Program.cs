f9023f0859f284ba7c2992d52801677bffdf5048
damara <damarmuhamad13@gmail.com>
2023-06-05 20:58:06 +0700

    update-program.cs

--- printer-windows/Program.cs
+++ printer-windows/Program.cs
@@ -1,5 +1,26 @@
 ﻿class Program {
     static void Main(string[] args){
-        
+     PrinterWindows printer;
+
+     Console.WriteLine("Pilih Printer : ");
+     Console.WriteLine("1. Epson");
+     Console.WriteLine("2. Canon");
+     Console.WriteLine("3. LasetJet\n");
+
+     Console.WriteLine("Nomor Printer [1..3] : ");
+     int nomorPrinter = Convert.ToInt32(Console.ReadLine());
+
+     if (nomorPrinter == 1){
+        printer = new Epson();
+     } else if (nomorPrinter == 2){
+        printer = new Canon();
+     } else{
+        printer = new LaserJet();
+     }
+
+     printer.Show();
+     printer.Print();
+
+     Console.ReadKey();   
     }
 }
