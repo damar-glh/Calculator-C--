df4873be590b3f81feb090b0565f29acafecdee5
damara <damarmuhamad13@gmail.com>
2023-06-12 17:50:54 +0700

    update-program.cs

--- LatihanInterface/Program.cs
+++ LatihanInterface/Program.cs
@@ -1,11 +1,12 @@
-﻿class Program{
+﻿// class Program
+class Program{
     public static void Main(string[] args){
-        IluasBangunDatar[] bangunDatar = new IluasBangunDatar[3];
-        bangunDatar[0] = new BujurSangkar(5);
+        IluasBangunDatar[] bangunDatar = new IluasBangunDatar[3]; // new object
+        bangunDatar[0] = new BujurSangkar(5); 
         bangunDatar[1] = new PersegiPanjang(4, 6);
         bangunDatar[2] = new Segitiga(3, 8);
 
-    foreach(BangunDatar bangun in  bangunDatar){
+    foreach(BangunDatar bangun in  bangunDatar){ // polymorphism
         bangun.Print();
     }
     }
