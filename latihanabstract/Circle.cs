d86caeae2e2f852e90617f08c5cb52c4996221de
damara <damarmuhamad13@gmail.com>
2023-06-19 14:15:50 +0700

    update-circle.cs

---
+++ latihanabstract/Circle.cs
@@ -0,0 +1,7 @@
+public class Circle : Shape{
+    public override void CalculateAre()
+    {
+        System.Console.WriteLine("Calculating area of a circle.");
+        // logika perhitungan luas lingkaran
+    }
+}
