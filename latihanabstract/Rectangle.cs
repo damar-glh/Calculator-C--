f026a97e14cdd24c803adf72e036d8d419883ff9
damara <damarmuhamad13@gmail.com>
2023-06-19 14:18:21 +0700

    update-rectangle.cs

---
+++ latihanabstract/Rectangle.cs
@@ -0,0 +1,7 @@
+public class Rectangle : Shape{
+    public override void CalculateAre()
+    {
+        System.Console.WriteLine("Calculating area of a rectangle.");
+        // logika perhitungan luas persegi panjang
+    }
+}
