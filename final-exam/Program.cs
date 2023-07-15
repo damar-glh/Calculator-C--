91386827e081dd987b8fb5e515f09abfe1489e3d
damara <damarmuhamad13@gmail.com>
2023-07-15 09:18:46 +0700

    update:add-class-Program

--- final-exam/Program.cs
+++ final-exam/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿public class Program {
+    public static void Main(string[] args) {
+        Player player = new Player("Player 1", 150);
+        Enemy enemy = new Enemy("Wizard", 100);
+
+        player.Attack();
+        enemy.Attack();
+
+        // Polymorphism: Menggunakan referensi kelas Character untuk objek player dan enemy
+        Character character1 = player;
+        Character character2 = enemy;
+
+        character1.Attack();
+        character2.Attack();
+    }
+}
