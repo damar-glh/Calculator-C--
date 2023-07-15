523ae6edd1289261687fc753c49bc0fdb56e0971
damara <damarmuhamad13@gmail.com>
2023-07-15 09:04:50 +0700

    update:add-abstrac-class

---
+++ final-exam/Character.cs
@@ -0,0 +1,6 @@
+// Abstraction: Mendefinisikan kelas abstrak sebagai dasar untuk kelas lainnya
+
+public abstract class Character {
+    public string Name {get; set;}
+    public abstract void Attack();
+}
