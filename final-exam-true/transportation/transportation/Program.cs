bb86fb040b46efd7d1a99ab410ff3079ea4a2e7f
damara <damarmuhamad13@gmail.com>
2023-07-16 20:08:36 +0700

    update:add-final-exam

--- final-exam-true/transportation/transportation/Program.cs
+++ final-exam-true/transportation/transportation/Program.cs
@@ -1,24 +1,36 @@
-﻿class Program
-{
-    static void Main(string[] args)
-    {
+﻿class Program {
+    static void Main(string[] args) {
         List<TicketTransportasi> tiketList = new List<TicketTransportasi>();
 
-        // menu
-        System.Console.WriteLine("==========================");
-        System.Console.WriteLine("=   Ticket Transportasi  =");
-        System.Console.WriteLine("==========================");
-        System.Console.WriteLine("1. Add Ticket");
-        System.Console.WriteLine("2. Delete Ticket");
-        System.Console.WriteLine("3. Ticket List");
-        System.Console.WriteLine("==========================");
-        System.Console.WriteLine("Enter your choice : ");
-        int choice = int.Parse(Console.Read());
-        switch (choice) {
-            case "1":
-                System.Console.WriteLine("Enter your name : ");
-                string name = Console.Read();
+        bool isReply = true;
+        while (isReply) {
+            // menu
+            System.Console.WriteLine("==========================");
+            System.Console.WriteLine("=   Ticket Transportasi  =");
+            System.Console.WriteLine("==========================");
+            System.Console.WriteLine("1. Add Ticket");
+            System.Console.WriteLine("2. Delete Ticket");
+            System.Console.WriteLine("3. Ticket List");
+            System.Console.WriteLine("==========================");
+            System.Console.WriteLine("Enter your choice : ");
+            int choice = int.Parse(Console.Read());
+            switch (choice) {
+                case "1":
+                    System.Console.WriteLine("Enter your name : ");
+                    string name = Console.Read();
+                    Console.Write("Enter your nik : ");
+                    long nik = long.Parse(Console.ReadLine());
+                    Console.Write("Enter your phone number : ");
+                    long phoneNumber = long.Parse(Console.ReadLine());
+                    Console.Write("Enter your veccine card id : ");
+                    string nomorKartuVaksin = Console.ReadLine();
+                    Console.Write("Enter your addres : ");
+                    string alamatRumah = Console.ReadLine();
+                    TicketTransportasi tiket = new TicketTransportasi(nama, nik, nomorTelepon, nomorKartuVaksin, alamatRumah);
+                    tiket.AddTicket(tiketList);
+                    break;
                 
+            }
         }
     }
 }
--- final-exam-true/transportation/transportation/TicketTransportasi.cs
+++ final-exam-true/transportation/transportation/TicketTransportasi.cs
@@ -1,34 +1,36 @@
 class TicketTransportion : ITicket {
     private string name;
-    private int nik;
-    private int phoneNumber;
-    private int veccineCardNumber;
+    private long nik;
+    private long phoneNumber;
+    private string veccineCardId;
     private string addres;
 
-    public TicketTransportion(string name, int nik, int phoneNumber, int veccineCardNumber, int addres) {
+    public TicketTransportion(string name, long nik, long phoneNumber, string veccineCardId, string addres) {
         this.name = name;
         this.nik = nik;
         this.phoneNumber = phoneNumber;
-        this.veccineCardNumber = veccineCardNumber;
+        this.veccineCardId = veccineCardId;
         this.addres = addres;
     }
 
     public void ticketInformation() {
-        Console.WriteLine("Informasi Tiket Transportasi");
+        Console.WriteLine("=====================================");
+        Console.WriteLine("= Transportation Ticket Information =");
+        Console.WriteLine("=====================================");
         Console.WriteLine("Name: " + name);
         Console.WriteLine("NIK: " + nik);
         Console.WriteLine("Phone Number: " + phoneNumber);
-        Console.WriteLine("Veccine Card Number: " + veccineCardNumber);
+        Console.WriteLine("Veccine Card Number: " + veccineCardId);
         Console.WriteLine("Addres: " + addres);
     }
 
     public void addTicket(List<TicketTransportion> ticketList) {
         ticketList.Add(this);
-        Console.WriteLine("Ticket add.");
+        Console.WriteLine("Ticket add !!!.");
     }
 
     public void deleteTicket(List<TicketTransportion> ticketList) {
         ticketList.Remove(this);
-        Console.WriteLine("Ticket delete.");
+        Console.WriteLine("Ticket delete !!!.");
     }
 }
