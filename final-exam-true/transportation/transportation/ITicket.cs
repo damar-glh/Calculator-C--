5f7d79a999d2fc01b1638fd2b52e08221d768794
damara <damarmuhamad13@gmail.com>
2023-07-16 20:59:56 +0700

    update:add-final-exam

--- final-exam-true/transportation/transportation/ITicket.cs
+++ final-exam-true/transportation/transportation/ITicket.cs
@@ -1,4 +1,7 @@
+// class interface untuk ticket
 interface ITicket {
+    // colections berupa List<TicketTransportion> tiketList 
+    // untuk menyimpan objek-objek dari class TicketTransportion
     void addTicket(List<TicketTransportion> tiketList);
     void deleteTicket(List<TicketTransportion> tiketList);
 }
--- final-exam-true/transportation/transportation/Program.cs
+++ final-exam-true/transportation/transportation/Program.cs
@@ -1,6 +1,6 @@
 ﻿class Program {
     static void Main(string[] args) {
-        List<TicketTransportasi> tiketList = new List<TicketTransportasi>();
+        List<TicketTransportion> tiketList = new List<TicketTransportion>();
 
         bool isReply = true;
         while (isReply) {
@@ -12,24 +12,57 @@
             System.Console.WriteLine("2. Delete Ticket");
             System.Console.WriteLine("3. Ticket List");
             System.Console.WriteLine("==========================");
-            System.Console.WriteLine("Enter your choice : ");
-            int choice = int.Parse(Console.Read());
+            System.Console.Write("Enter your choice : ");
+            int choice = int.Parse(Console.ReadLine());
             switch (choice) {
-                case "1":
-                    System.Console.WriteLine("Enter your name : ");
-                    string name = Console.Read();
+                case 1:
+                    System.Console.Write("Enter your name : ");
+                    string name = Console.ReadLine();
                     Console.Write("Enter your nik : ");
                     long nik = long.Parse(Console.ReadLine());
                     Console.Write("Enter your phone number : ");
                     long phoneNumber = long.Parse(Console.ReadLine());
                     Console.Write("Enter your veccine card id : ");
-                    string nomorKartuVaksin = Console.ReadLine();
+                    string veccineCardId = Console.ReadLine();
                     Console.Write("Enter your addres : ");
-                    string alamatRumah = Console.ReadLine();
-                    TicketTransportasi tiket = new TicketTransportasi(nama, nik, nomorTelepon, nomorKartuVaksin, alamatRumah);
-                    tiket.AddTicket(tiketList);
+                    string addres = Console.ReadLine();
+                    TicketTransportion ticket = new TicketTransportion(name, nik, phoneNumber, veccineCardId, addres);
+                    ticket.addTicket(tiketList);
                     break;
-                
+                case 2:
+                    if (tiketList.Count > 0) {
+                        Console.WriteLine("=== Tiket saat ini ===");
+                        for (int i = 0; i < tiketList.Count; i++) {
+                            Console.WriteLine($"Tiket {i + 1}:");
+                            tiketList[i].ticketInformation();
+                            Console.WriteLine();
+                        }
+                        
+                        Console.Write("Pilih tiket yang akan dihapus (1-" + tiketList.Count + "): ");
+                        int nomorTiket = int.Parse(Console.ReadLine());
+
+                        if (nomorTiket >= 1 && nomorTiket <= tiketList.Count) {
+                            TicketTransportion tiketTerpilih = tiketList[nomorTiket - 1];
+                            tiketTerpilih.deleteTicket(tiketList);
+                        } else {
+                            Console.WriteLine("Tiket tidak valid!");
+                        }
+                    } else {
+                        Console.WriteLine("Belum ada tiket yang tersedia!");
+                    }
+                    break;
+                case 3:
+                    if (tiketList.Count > 0) {
+                        Console.WriteLine("=== Tiket saat ini ===");
+                        for (int i = 0; i < tiketList.Count; i++) {
+                            Console.WriteLine($"Tiket {i + 1}:");
+                            tiketList[i].ticketInformation();
+                            Console.WriteLine();
+                        }
+                    } else {
+                        Console.WriteLine("Belum ada tiket yang tersedia!");
+                    }
+                    break;
             }
         }
     }
