de3b1f385bebca0800ed0899c17910698de984b4
damara <damarmuhamad13@gmail.com>
2023-09-07 12:16:39 +0700

    update: all-program

---
+++ CalculateApp/CalculatorWindowsFormsApp/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace CalculatorWindowsFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.B = new System.Windows.Forms.Label();
+            this.txtNilaiA = new System.Windows.Forms.TextBox();
+            this.txtNilaiB = new System.Windows.Forms.TextBox();
+            this.btnHitung = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lstHasil = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F);
+            this.label1.Location = new System.Drawing.Point(46, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nilai A";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // B
+            // 
+            this.B.AutoSize = true;
+            this.B.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F);
+            this.B.Location = new System.Drawing.Point(46, 63);
+            this.B.Name = "B";
+            this.B.Size = new System.Drawing.Size(62, 22);
+            this.B.TabIndex = 1;
+            this.B.Text = "Nilai B";
+            // 
+            // txtNilaiA
+            // 
+            this.txtNilaiA.Location = new System.Drawing.Point(137, 24);
+            this.txtNilaiA.Name = "txtNilaiA";
+            this.txtNilaiA.Size = new System.Drawing.Size(153, 22);
+            this.txtNilaiA.TabIndex = 2;
+            // 
+            // txtNilaiB
+            // 
+            this.txtNilaiB.Location = new System.Drawing.Point(137, 63);
+            this.txtNilaiB.Name = "txtNilaiB";
+            this.txtNilaiB.Size = new System.Drawing.Size(153, 22);
+            this.txtNilaiB.TabIndex = 3;
+            this.txtNilaiB.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // btnHitung
+            // 
+            this.btnHitung.Location = new System.Drawing.Point(137, 138);
+            this.btnHitung.Name = "btnHitung";
+            this.btnHitung.Size = new System.Drawing.Size(127, 41);
+            this.btnHitung.TabIndex = 4;
+            this.btnHitung.Text = "Hitung";
+            this.btnHitung.UseVisualStyleBackColor = true;
+            this.btnHitung.Click += new System.EventHandler(this.btnHitung_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lstHasil);
+            this.groupBox1.Location = new System.Drawing.Point(50, 204);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(425, 234);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "[ hasil ]";
+            // 
+            // lstHasil
+            // 
+            this.lstHasil.FormattingEnabled = true;
+            this.lstHasil.ItemHeight = 16;
+            this.lstHasil.Location = new System.Drawing.Point(18, 21);
+            this.lstHasil.Name = "lstHasil";
+            this.lstHasil.Size = new System.Drawing.Size(389, 196);
+            this.lstHasil.TabIndex = 6;
+            this.lstHasil.SelectedIndexChanged += new System.EventHandler(this.lstHasil_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(521, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnHitung);
+            this.Controls.Add(this.txtNilaiB);
+            this.Controls.Add(this.txtNilaiA);
+            this.Controls.Add(this.B);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Aplikasi Calculator Versi Windows";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label B;
+        private System.Windows.Forms.TextBox txtNilaiA;
+        private System.Windows.Forms.TextBox txtNilaiB;
+        private System.Windows.Forms.Button btnHitung;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox lstHasil;
+    }
+}
+
