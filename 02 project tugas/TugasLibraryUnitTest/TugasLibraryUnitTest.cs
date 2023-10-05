fd94e943789088d77e3acde3171ec047afe9399a
damara <damarmuhamad13@gmail.com>
2023-10-05 13:10:03 +0700

    add(unit-testing): using the NUnit library to perform unit testing

---
+++ 02 project tugas/TugasLibraryUnitTest/TugasLibraryUnitTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TugasLibrary;
+using NUnit.Framework;
+
+namespace TugasLibraryUnitTest
+{
+    [TestFixture]
+    public class TugasLibraryUnitTest
+    {
+        private Day day;
+        private User user;
+
+        [SetUp]
+        public void Init()
+        {
+            day = new Day();
+            user = new User();
+        }
+
+        [Test]
+        public void DayTest()
+        {
+            Assert.AreEqual("Senin", day.NameOfDay(1)); 
+        }
+
+        [Test]
+        public void UserTest()
+        {
+            Assert.IsTrue(user.IsValidUser("admin", "admin"));
+            Assert.IsFalse(user.IsValidUser("admin", "adefddmin"));
+        }
+    }
+}
