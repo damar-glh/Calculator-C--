44cc0256f191ba2a55a91f6882e3b09951fb2432
damara <damarmuhamad13@gmail.com>
2023-11-15 22:25:28 +0700

    add(deeh): delegate-event-eventhandler

---
+++ delegate-event-eventhandler/delegate-event-eventhandler/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace delegate_event_eventhandler
+{
+    public delegate void MahasiswaAddedHandler(string mahasiswa);
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_MahasiswaAdded(string mahasiswa)
+        {
+            // add mahasiswa ke dalam listBox
+            listBox1.Items.Add(mahasiswa);
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Form2 frm = new Form2();
+            frm.MahasiswaAdded += Form1_MahasiswaAdded;
+            frm.ShowDialog();
+        }
+    }
+}
---
+++ delegate-event-eventhandler/delegate-event-eventhandler/Form2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace delegate_event_eventhandler
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        public event MahasiswaAddedHandler MahasiswaAdded;
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            string namaMahasiswa = textBox1.Text;
+            MahasiswaAdded?.Invoke(namaMahasiswa);
+        }
+    }
+}
